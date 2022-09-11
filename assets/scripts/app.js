da2faa28883613687748f683e365eb9a715ff599
farildev <memmedovfaril@gmail.com>
2022-09-11 22:13:04 +0400

    v1.0.2

--- assets/scripts/app.js
+++
@@ -1,8 +0,0 @@
-const btn = document.querySelector("#search-btn");
-const searchInput = document.querySelector("#country-input");
-
-btn.addEventListener("click", () => {
-    let countryName = searchInput.value;
-    let url = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
-    console.log(url);
-});
