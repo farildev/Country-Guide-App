0435ae0cd2df2d98a17f855401a0cbf67a5919c6
farildev <memmedovfaril@gmail.com>
2022-09-12 13:53:21 +0400

    v1.0.6

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,8 +1,8 @@
 let seacrhBtn = document.getElementById("search-btn");
-let countryInp = document.getElementById("country-input");
+let countryInput = document.getElementById("country-input");
 
-seacrhBtn.addEventListener('click' , () => {
-    let countryName = 'France';
+seacrhBtn.addEventListener("click" , () => {
+    let countryName = "Spain";
     let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
     console.log(finalURL);
     fetch(finalURL)
@@ -16,7 +16,34 @@ seacrhBtn.addEventListener('click' , () => {
         console.log(Object.keys(data[0].currencies)[0]);
         console.log(data[0].currencies[Object.keys(data[0].currencies)].name);
         console.log(Object.values(data[0].languages).toString().split(",").join(", "));
-
+        result.innerHTML = `<img src="${data[0].flags.svg}" class="flag-img" />
+        <h2>${data[0].name.common}</h2>
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Capital: <span>${data[0].capital[0]}</span></h4>
+            </div>
+        </div>
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Continent: <span>${data[0].continents[0]}</span></h4>
+            </div>
+        </div>
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Population: <span>${data[0].population}</span></h4>
+            </div>
+        </div>
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Currency: <span>${data[0].currencies[Object.keys(data[0].currencies)].name} - ${Object.keys(data[0].currencies)[0]}</span></h4>
+            </div>
+        </div>
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Common languages: <span> ${Object.values(data[0].languages).toString().split(",").join(", ")}</span></h4>
+            </div>
+        </div>
+        `;
     })
 });
 
