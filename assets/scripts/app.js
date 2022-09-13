454f4caf68c6baf60633a27bdfd59130849fa222
farildev <memmedovfaril@gmail.com>
2022-09-13 11:12:42 +0400

    v1.0.7

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,20 +2,11 @@ let seacrhBtn = document.getElementById("search-btn");
 let countryInput = document.getElementById("country-input");
 
 seacrhBtn.addEventListener("click" , () => {
-    let countryName = "Spain";
+    let countryName = countryInput.value('');
     let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
-    console.log(finalURL);
     fetch(finalURL)
     .then((response) => response.json())
     .then((data) => {
-        console.log(data[0]);
-        console.log(data[0].capital[0]);
-        console.log(data[0].flags.svg);
-        console.log(data[0].name.common);
-        console.log(data[0].continents[0]);
-        console.log(Object.keys(data[0].currencies)[0]);
-        console.log(data[0].currencies[Object.keys(data[0].currencies)].name);
-        console.log(Object.values(data[0].languages).toString().split(",").join(", "));
         result.innerHTML = `<img src="${data[0].flags.svg}" class="flag-img" />
         <h2>${data[0].name.common}</h2>
         <div class="wrapper">
@@ -44,6 +35,11 @@ seacrhBtn.addEventListener("click" , () => {
             </div>
         </div>
         `;
+    })
+    .catch(() => {
+        if(countryName.length == 0) {
+            result.innerHTML = `<h3>Input the field cannot be empty</h3>`
+        }
     })
 });
 
